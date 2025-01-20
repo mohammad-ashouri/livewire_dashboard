bebb9d70542aaaca9eea67ce35b2f0bca9171ff4
SH <m.ashouri.wdev@gmail.com>
2025-01-20 09:15:39 +0330

    Fix preloader for dark/light mode

--- resources/views/livewire/partials/preloader.blade.php
+++ resources/views/livewire/partials/preloader.blade.php
@@ -4,11 +4,9 @@
     x-init="window.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => loaded = false, 2000);
     })"
-    class="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-gradient-to-br from-gray-800 via-gray-900 to-black"
+    class="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center bg-white dark:bg-black"
 >
     <div class="relative flex items-center justify-center h-24 w-24">
-        <span class="loading loading-infinity loading-lg"></span>
+        <span class="loading loading-infinity loading-lg dark:text-white text-black"></span>
     </div>
 </div>
-
-
